09040a80f676be8f95eb915164685e478601a596
Johan Pettersson <xsnudelx@hotmail.com>
2015-09-26 15:46:06 +0200

    Initial commit

---
+++ src/Client/Client.java
@@ -0,0 +1,11 @@
+package Client;
+
+/**
+ * Assignment 2B.
+ * Created by Peonsson and roppe546 on 2015-09-26.
+ */
+public class Client {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
---
+++ src/Server/Server.java
@@ -0,0 +1,12 @@
+package Server;
+
+/**
+ * Assignment 2B.
+ * Created by Peonsson and roppe546 on 2015-09-26.
+ */
+public class Server {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
