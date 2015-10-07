400be993e4c4662ce73b79c7744dd3599d0535c3
johanpettersson <xsnudelx@hotmail.com>
2015-10-07 13:06:39 +0200

    init

---
+++ src/Peer/Peer.java
@@ -0,0 +1,9 @@
+package Peer;
+
+/**
+ * Created by Johan Pettersson on 2015-10-07 13:06.
+ * At Kungliga Tekniska Högskolan (KTH) in Sweden.
+ * Contact: johanp7@kth.se
+ */
+public class Peer {
+}
