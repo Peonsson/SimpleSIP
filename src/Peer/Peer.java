7bb9c2748bf30da62440002c08e574c8bc0ffa15
Peonsson <xsnudelx@hotmail.com>
2015-10-08 18:48:17 +0200

    started on skeletoncode

--- src/Peer/Peer.java
+++ src/Peer/Peer.java
@@ -2,7 +2,10 @@ package Peer;
 
 import com.sun.corba.se.spi.activation.Server;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
 
@@ -18,16 +21,31 @@ public class Peer {
     public static void main(String[] args) {
 
         StateHandler handler = new StateHandler();
-        new ClientHandler(busy).start();
+        new ClientHandler(busy, handler).start();
 
         try {
             ServerSocket listenSocket = new ServerSocket(SERVER_PORT);
             Socket clientSocket = listenSocket.accept();
             handler.invokeGotInvite(); // goes from notConnected to connecting
 
+            //TODO: implement logic for going from connecting to not connected.
+            PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
+//            out.println("TRYING");
+//            out.println("RINGING");
+            out.println("OK");
+
+            handler.invokeTryConnect(); // goes from connecting to wait ack
+
+            BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+
+            String response = in.readLine();
+            if(response.equals("ACK")) {
+                handler.invokeGotAck(); // goes from wait ack to connected
+                //TODO: implement audio shit.
+                out.println("we are not connected, yo");
 
 
-
+            }
 
 
 
